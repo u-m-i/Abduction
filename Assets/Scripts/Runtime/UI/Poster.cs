b8907693728469ac06f3bafcdbff7b3062aaca82
umi <asiessoyeltier1@gmail.com>
2023-06-02 00:16:01 -0500

    Universal RP updated to 14.0.7

---
+++ Assets/Scripts/Runtime/UI/Poster.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Abduction
+{
+    public class Poster : MonoBehaviour
+    {
+
+        /// 
+        private void OnMouseOver()
+        {
+            Debug.Log("Click E for this shit to appear");
+        }
+    }
+}
